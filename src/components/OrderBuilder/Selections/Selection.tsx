899cf359ae7d81fc17b6155abd2413eb335e31d7
Hoang <hoang.viet.nguyen@outlook.com>
2020-09-09 21:39:37 -0400

    added images of ingredients to selection, added overlay that toggles on hover of ingredients, refactored selections

--- src/components/OrderBuilder/Selections/Selection.tsx
+++ src/components/OrderBuilder/Selections/Selection.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core/';
-import { styled } from '@material-ui/core/styles';
+import { styled, useTheme } from '@material-ui/core/styles';
 
-const StyledCard = styled(Card)({});
+const StyledImage = styled('img')({
+  display: 'block',
+  width: '100%',
+});
+
+const StyledCard = styled(Card)({
+  position: 'relative',
+});
+
+const Overlay = styled(({ show, color, ...other }) => <div {...other}></div>)({
+  position: 'absolute',
+  top: '0',
+  right: '0',
+  left: '0',
+  bottom: '0',
+  zIndex: 1,
+  backgroundColor: (props: { show: boolean; color: string }) => props.color,
+  opacity: (props: { show: boolean; color: string }) =>
+    props.show ? '0.3' : '0',
+  cursor: 'pointer',
+});
 
 type Props<T> = {
   name: string;
@@ -11,13 +31,29 @@ type Props<T> = {
 };
 
 const Selection = <T extends {}>({ name, image, id }: Props<T>) => {
+  const theme = useTheme();
+  const [overlay, setOverlay] = useState(false);
+  const handleMouseEnter = (e: React.MouseEvent) => {
+    setOverlay(true);
+  };
+
+  const handleMouseExit = (e: React.MouseEvent) => {
+    setOverlay(false);
+  };
   return (
     <Grid item xs={3}>
-      <Card>
+      <StyledCard
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseExit}
+      >
         <CardContent>
-          <Typography>{name}</Typography>
+          <Typography align='center' variant='h6'>
+            {name}
+          </Typography>
         </CardContent>
-      </Card>
+        <StyledImage src={image} alt=''></StyledImage>
+        <Overlay show={overlay} color={theme.palette.primary.light}></Overlay>
+      </StyledCard>
     </Grid>
   );
 };
